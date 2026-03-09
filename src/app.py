f6e34850f71964e1261684ec431a3885aa32441d
CLARA VECCHIO MACHADO DA SILVA <1051392421023@fatec.corp>
2026-03-09 09:17:40 -0300

    Melhora descrição da rota de cadastro

--- src/app.py
+++ src/app.py
@@ -36,3 +36,7 @@ def contato():
 @app.route("/cadastro-livro")
 def cadastro_livro():
     return "Página de cadastro de livros"
+
+@app.route("/cadastro-livro")
+def cadastro_livro():
+    return "Formulário de cadastro de livros"
